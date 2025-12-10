ee51b608b34ce0930d757fc01628f3c2d6ad88f0
Nguyennhi-09 <npnhi.25174600181@sv.uneti.edu.vn>
2025-12-10 22:57:11 +0700

    Update bai8.py

---
+++ 35_NguyenPhuongNhi_THCS/bai8.py
@@ -0,0 +1,9 @@
+def tim_so_le_lon_nhat(a,b,c):
+    so_lon_nhat = -1
+    if a % 2 != 0 :
+        so_lon_nhat = a
+    if b % 2 != 0 :
+        so_lon_nhat = b 
+    if c % 2 != 0 :
+        so_lon_nhat = c
+    return so_lon_nhat
